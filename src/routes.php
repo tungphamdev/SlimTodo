87ee0a3baf631c3ba80dd7448b86362b46e86eec
Tung Pham Thanh <tungpham@HarveyNash.vn.local>
2017-11-21 17:48:58 +0700

    update, delete

--- src/routes.php
+++ src/routes.php
@@ -6,39 +6,62 @@ use Slim\Http\Response;
 
 // Routes
 
+/*
+    Get all todos
+*/
 $app->get('/gettodos', function (Request $request, Response $response, array $args) {
-    // Sample log message
+    // log message
     $this->logger->info("Slim-Skeleton '/' route");
     $data = $this->database->select('todo', ['id', 'name', 'done']);
-    // Render index view
-    // return $this->renderer->render($response, 'index.phtml', $args);
     return $response->withJSON($data);
 });
 
+
+/*
+    Insert new todo
+*/
 $app->post('/inserttodo', function (Request $request, Response $response, array $args) {
-    // Sample log message
+    // log message
     $this->logger->info("Slim-Skeleton '/' route");
     $data = $request->getParsedBody();
-
-    // var_dump($data["name"]);
-    // $data2 = json_decode($data);
-    //return $response->write($data->name);
     $this->database->insert("todo", [
         "name" => $data["name"],
         "done" => "false"
     ]);
 });
 
-$app->post('/updatetodo', function (Request $request, Response $response, array $args) {
+
+/*
+    Update todo
+*/
+$app->post('/updatetodo/{id}', function (Request $request, Response $response, array $args) {
+    // log message
+    $this->logger->info("Slim-Skeleton '/' route");
+    $data = $request->getParsedBody();
+    $route = $request->getAttribute('route');
+    $todoId = $route->getArgument('id');
+    $this->database->update("todo", [
+        "name" => $data["name"],
+        "done" => $data["done"]
+    ], [
+        "id" => $todoId
+    ]);
+});
+
+
+/*
+    Delete todo
+*/
+$app->post('/deletetodo/{id}', function (Request $request, Response $response, array $args) {
     // Sample log message
     $this->logger->info("Slim-Skeleton '/' route");
     $data = $request->getParsedBody();
-
-    // var_dump($data["name"]);
+    $route = $request->getAttribute('route');
+    $todoId = $route->getArgument('id');
+    // var_dump($data);
     // $data2 = json_decode($data);
     //return $response->write($data->name);
-    $this->database->insert("todo", [
-        "name" => $data["name"],
-        "done" => "false"
+    $this->database->delete("todo", [
+        "id" =>$todoId
     ]);
 });
